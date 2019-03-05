ca262c8efff993ac0fee9d84782fed6091b2efe1
helme__c <claire.helme_guizon@epita.fr>
2019-03-05 23:06:50 +0100

    [SERV] Parsing the json body request to Measurement Format

--- app/controllers/MeasurementController.scala
+++ app/controllers/MeasurementController.scala
@@ -1,8 +1,10 @@
 package controllers
 
 import javax.inject._
-import play.api._
+import play.api.Logger
+import play.api.libs.json._
 import play.api.mvc._
+import services.{GpsDD, Measurement}
 
 /**
   * This controller creates an `Action` to handle HTTP requests to the
@@ -11,10 +13,19 @@ import play.api.mvc._
 @Singleton
 class MeasurementController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  def postData() = Action { implicit request: Request[AnyContent] =>
-    println(request.body)
-    Ok("bonjour")
+  def postData = Action(parse.json) { request =>
+    implicit val gpsDDFormat: Format[GpsDD] = Json.format[GpsDD]
+    implicit val measurementFormat: Format[Measurement]= Json.format[Measurement]
+    val measurementResult = request.body.validate[Measurement]
+    measurementResult.fold(
+      errors => {
+        BadRequest(Json.obj("status" -> "KO", "message" -> JsError.toJson(errors)))
+      },
+      measurement => {
+        Logger.debug(s"Successfully build $measurement")
+        measurement.save()
+        Ok("Measurement added\n")
+      }
+    )
   }
-
-
 }
