dd3fc5eaacd7f4f3ad3a94329715c5c25dc89c6d
Croupile <fiona.carbonell@gmail.com>
2019-03-07 19:44:53 +0100

    [CLIENT] create file and check extension

---
+++ app/client/FileReader.scala
@@ -0,0 +1,36 @@
+import java.io.File
+import java.nio.file.{Files, Paths}
+
+def exists(dir: String) : Boolean = {
+  Files.exists(Paths.get(dir))
+}
+
+def getListOfFiles(dir: File): List[File] = { dir.listFiles(_.isFile).toList }
+
+def checkExtension(fname: File) = fname.getName.drop(fname.getName.lastIndexOf(".")) match {
+  case ".json" => println(fname) // parseCsv()
+  case ".csv" => println(fname) // parseJson()
+  case _ => System.err.println("Bad file extension")
+}
+
+// def parseCsv(file: File)
+
+// def parseJson(file: File)
+
+def main(directory: String) = {
+  val dir = new File(directory)
+  println(dir.exists())
+  println(dir.isDirectory)
+  println(Files.exists(Paths.get(directory)))
+  if (Files.exists(Paths.get(directory))) {
+
+    val list = getListOfFiles(dir)
+
+    list.foreach(f => checkExtension(f))
+
+  }
+
+
+}
+
+main("parsetest")
