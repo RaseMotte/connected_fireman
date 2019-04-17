f8cd14278139ed3a643451a88f78a1cc857fa980
helme__c <claire.helme_guizon@epita.fr>
2019-04-17 15:29:35 +0200

    [SERV] - debug print

--- app/services/Measurement.scala
+++ app/services/Measurement.scala
@@ -2,7 +2,6 @@ package services
 
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
-import javax.inject._
 
 import play.api.Logger
 
@@ -10,14 +9,14 @@ import play.api.Logger
   * Represent a measure as sent from the device.
   *
   * @param udid: Srting - user device id
-  * @param gpsDD: GpsDD - coordinates of the fireman at a given time
+  * @param longitude: Double -
+  * @param latitude: Double -
   * @param temperatureIn: Float - temperature inside the fireman's suit
   * @param temperatureOut: float - temperature outside the fireman's suit
-  * @param time: Timestamp - time at which the measurement was done, follows ISO 8601
+  * @param mtime: Timestamp - time at which the measurement was done, follows ISO 8601
   */
 case class Measurement (udid: String, longitude: Double, latitude: Double,
                         temperatureIn: Float, temperatureOut: Float, mtime: String) {
-
 
   def sqlFormat = s"($udid, $longitude, $latitude, $temperatureIn, $temperatureOut, TIMESTAMP'${this.getTime()}')"
 
